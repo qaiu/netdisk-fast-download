1651f1ea93cd2042cd2817d461ac28eb1e9ee0b4
QAIU <736226400@qq.com>
2023-04-25 17:51:39 +0800

    do var

--- bin/run.bat
+++ bin/run.bat
@@ -1,5 +1,5 @@
 @echo off && @chcp 65001 > nul
 pushd %~dp0
 set LIB_DIR=%~dp0
-for /f "delims=X" %%i in ('dir /b %LIB_DIR%\web-*.jar') do set LAUNCH_JAR=%LIB_DIR%\%%i
+for /f "delims=X" %%i in ('dir /b %LIB_DIR%\lz-cow-api-web-*.jar') do set LAUNCH_JAR=%LIB_DIR%\%%i
 "%JAVA_HOME%\bin\java.exe" -Xmx512M -Dfile.encoding=utf8 -jar %LAUNCH_JAR% %*
