a14db13485e453239ce015d02aa5728ae46a5413
QAIU <736226400@qq.com>
2023-06-08 17:33:42 +0800

    其他网盘API

---
+++ web-service/src/main/java/cn/qaiu/lz/common/util/QkTool.java
@@ -0,0 +1,12 @@
+package cn.qaiu.lz.common.util;
+
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+
+public class QkTool {
+    public static Future<String> parse(String data, String code) {
+        Promise<String> promise = Promise.promise();
+
+        return promise.future();
+    }
+}
