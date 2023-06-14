2158be14cad965973c39bf26aca06838c4e1cedb
qaiu <736226400@qq.com>
2023-06-14 10:30:16 +0800

    修复Linux运行脚本

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 # set -x
-LAUNCH_JAR="netdisk-fast-download-*.jar"
+LAUNCH_JAR="netdisk-fast-download-0.1.5.jar"
 nohup java -Xmx512M -jar "$LAUNCH_JAR" "$@" >startup.log 2>&1 &
 tail -f startup.log
+
