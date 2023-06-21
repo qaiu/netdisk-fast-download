9d3fce3deeeeecd8efa7503f039a98731a105c12
QAIU <736226400@qq.com>
2023-06-21 16:49:01 +0800

    解决一些 warning jdk14对可序列化对象添加@Serial

--- core/src/main/java/cn/qaiu/vx/core/model/JsonResult.java
+++ core/src/main/java/cn/qaiu/vx/core/model/JsonResult.java
@@ -3,6 +3,7 @@ package cn.qaiu.vx.core.model;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 /**
@@ -13,6 +14,7 @@ import java.io.Serializable;
  */
 public class JsonResult<T> implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     private static final int SUCCESS_CODE = 200;
