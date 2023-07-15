1ae2c89d4fb6677a4ea2a83b3441b48d20c57a9d
qaiu <736226400@qq.com>
2023-07-16 03:11:41 +0800

    - 更新版本号: 0.1.5->0.1.6

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # set -x
-LAUNCH_JAR="netdisk-fast-download-0.1.5.jar"
+LAUNCH_JAR="netdisk-fast-download-0.1.6.jar"
 nohup java -Xmx512M -jar "$LAUNCH_JAR" "$@" >startup.log 2>&1 &
 tail -f startup.log
 
