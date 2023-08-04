facf9b645ec206c23e5c970c57f090b7d8f9666f
QAIU <736226400@qq.com>
2023-08-04 17:49:30 +0800

    0

--- web-service/src/main/java/cn/qaiu/lz/common/parser/IPanTool.java
+++ web-service/src/main/java/cn/qaiu/lz/common/parser/IPanTool.java
@@ -4,16 +4,16 @@ import cn.qaiu.lz.common.parser.impl.*;
 import io.vertx.core.Future;
 
 public interface IPanTool {
-    Future<String> parse(String data, String code);
+    Future<String> parse();
 
-    static IPanTool typeMatching(String type) {
+    static IPanTool typeMatching(String type, String key, String pwd) {
         return switch (type) {
             case "lz" -> new LzTool();
             case "cow" -> new CowTool();
             case "ec" -> new EcTool();
             case "fc" -> new FcTool();
             case "uc" -> new UcTool();
-            case "ye" -> new YeTool();
+            case "ye" -> new YeTool(key, pwd);
             case "fj" -> new FjTool();
             default -> {
                 throw new IllegalArgumentException("未知分享类型");
@@ -21,7 +21,7 @@ public interface IPanTool {
         };
     }
 
-    static IPanTool shareURLPrefixMatching(String url) {
+    static IPanTool shareURLPrefixMatching(String url, String pwd) {
 
         if (url.startsWith(CowTool.SHARE_URL_PREFIX)) {
             return new CowTool();
@@ -32,7 +32,7 @@ public interface IPanTool {
         } else if (url.startsWith(UcTool.SHARE_URL_PREFIX)) {
             return new UcTool();
         } else if (url.startsWith(YeTool.SHARE_URL_PREFIX)) {
-            return new YeTool();
+            return new YeTool(url, pwd);
         } else if (url.startsWith(FjTool.SHARE_URL_PREFIX)) {
             return new FjTool();
         } else if (url.contains("lanzou")) {
