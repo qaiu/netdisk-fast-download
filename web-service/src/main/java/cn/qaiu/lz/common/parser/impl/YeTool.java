b78a8300c677ba76aa478ff20d4322de050c8a9a
QAIU <736226400@qq.com>
2023-08-04 17:49:30 +0800

    0

--- web-service/src/main/java/cn/qaiu/lz/common/parser/impl/YeTool.java
+++ web-service/src/main/java/cn/qaiu/lz/common/parser/impl/YeTool.java
@@ -1,22 +1,18 @@
 package cn.qaiu.lz.common.parser.impl;
 
 import cn.qaiu.lz.common.parser.IPanTool;
+import cn.qaiu.lz.common.parser.PanBase;
 import cn.qaiu.lz.common.util.CommonUtils;
 import cn.qaiu.lz.common.util.JsExecUtils;
 import cn.qaiu.lz.common.util.PanExceptionUtils;
 import cn.qaiu.vx.core.util.VertxHolder;
 import io.vertx.core.Future;
-import io.vertx.core.Promise;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.uritemplate.UriTemplate;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.openjdk.nashorn.api.scripting.ScriptObjectMirror;
 
-import javax.script.ScriptException;
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.Base64;
 import java.util.Map;
@@ -26,8 +22,8 @@ import java.util.regex.Pattern;
 /**
  * 123网盘
  */
-@Slf4j
-public class YeTool implements IPanTool {
+public class YeTool extends PanBase implements IPanTool {
+
     public static final String SHARE_URL_PREFIX = "https://www.123pan.com/s/";
     public static final String FIRST_REQUEST_URL = SHARE_URL_PREFIX + "{key}.html";
 /*
@@ -45,10 +41,13 @@ public class YeTool implements IPanTool {
             "&shareKey={shareKey}&SharePwd={pwd}&ParentFileId=0&Page=1&event=homeListFile&operateType=1";
     private static final String DOWNLOAD_API_URL = "https://www.123pan.com/b/api/share/download/info?{authK}={authV}";
 
-    public Future<String> parse(String data, String code) {
+    public YeTool(String key, String pwd) {
+        super(key, pwd);
+    }
 
-        String dataKey = CommonUtils.adaptShortPaths(SHARE_URL_PREFIX, data);
-        Promise<String> promise = Promise.promise();
+    public Future<String> parse() {
+
+        String dataKey = CommonUtils.adaptShortPaths(SHARE_URL_PREFIX, key);
         WebClient client = WebClient.create(VertxHolder.getVertxInstance());
 
         client.getAbs(UriTemplate.of(FIRST_REQUEST_URL)).setTemplateParam("key", dataKey).send().onSuccess(res -> {
@@ -58,7 +57,7 @@ public class YeTool implements IPanTool {
             Matcher matcher = compile.matcher(html);
 
             if (!matcher.find()) {
-                promise.fail(html + "\n Ye: " + dataKey + " 正则匹配失败");
+                fail(html + "\n Ye: " + dataKey + " 正则匹配失败");
                 return;
             }
             String fileInfoString = matcher.group(1);
@@ -67,45 +66,45 @@ public class YeTool implements IPanTool {
             JsonObject resListJson = fileInfoJson.getJsonObject("reslist");
 
             if (resJson == null || resJson.getInteger("code") != 0) {
-                promise.fail(dataKey + " 解析到异常JSON: " + resJson);
+                fail(dataKey + " 解析到异常JSON: " + resJson);
                 return;
             }
             String shareKey = resJson.getJsonObject("data").getString("ShareKey");
             if (resListJson == null || resListJson.getInteger("code") != 0) {
                 // 加密分享
-                if (StringUtils.isNotEmpty(code)) {
+                if (StringUtils.isNotEmpty(pwd)) {
                     client.getAbs(UriTemplate.of(GET_FILE_INFO_URL))
                             .setTemplateParam("shareKey", shareKey)
-                            .setTemplateParam("pwd", code)
+                            .setTemplateParam("pwd", pwd)
                             // .setTemplateParam("authKey", AESUtils.getAuthKey("/b/api/share/get"))
                             .putHeader("Platform", "web")
                             .putHeader("App-Version", "3")
                             .send().onSuccess(res2 -> {
                                 JsonObject infoJson = res2.bodyAsJsonObject();
                                 if (infoJson.getInteger("code") != 0) {
-                                    promise.fail("Ye: " + dataKey + " 状态码异常" + infoJson);
+                                    fail("{} 状态码异常 {}", dataKey, infoJson);
                                     return;
                                 }
                                 JsonObject getFileInfoJson =
                                         infoJson.getJsonObject("data").getJsonArray("InfoList").getJsonObject(0);
                                 getFileInfoJson.put("ShareKey", shareKey);
-                                getDownUrl(promise, client, getFileInfoJson);
-                            }).onFailure(t -> promise.fail(PanExceptionUtils.fillRunTimeException("Ye", dataKey, t)));
+                                getDownUrl(client, getFileInfoJson);
+                            }).onFailure(this.handleFail("获取文件信息失败"));
                 } else {
-                    promise.fail(dataKey + " 该分享需要密码");
+                    fail(dataKey + " 该分享需要密码");
                 }
                 return;
             }
 
             JsonObject reqBodyJson = resListJson.getJsonObject("data").getJsonArray("InfoList").getJsonObject(0);
             reqBodyJson.put("ShareKey", shareKey);
-            getDownUrl(promise, client, reqBodyJson);
-        }).onFailure(t -> promise.fail(PanExceptionUtils.fillRunTimeException("Ye", dataKey, t)));
+            getDownUrl(client, reqBodyJson);
+        }).onFailure(this.handleFail(""));
 
         return promise.future();
     }
 
-    private static void getDownUrl(Promise<String> promise, WebClient client, JsonObject reqBodyJson) {
+    private void getDownUrl(WebClient client, JsonObject reqBodyJson) {
         log.info(reqBodyJson.encodePrettily());
         JsonObject jsonObject = new JsonObject();
         // {"ShareKey":"iaKtVv-6OECd","FileID":2193732,"S3keyFlag":"1811834632-0","Size":4203111,
@@ -120,12 +119,8 @@ public class YeTool implements IPanTool {
         ScriptObjectMirror getSign;
         try {
             getSign = JsExecUtils.executeJs("getSign", "/b/api/share/download/info");
-        } catch (ScriptException | IOException | NoSuchMethodException e) {
-            promise.fail(e);
-            return;
-        }
-        if (getSign == null) {
-            promise.fail(ArrayUtils.toString(getSign));
+        } catch (Exception e) {
+            fail(e, "JS函数执行异常");
             return;
         }
         log.info("ye getSign: {}={}", getSign.get("0").toString(), getSign.get("1").toString());
@@ -140,11 +135,11 @@ public class YeTool implements IPanTool {
 
                     try {
                         if (downURLJson.getInteger("code") != 0) {
-                            promise.fail("Ye: downURLJson返回值异常->" + downURLJson);
+                            fail("Ye: downURLJson返回值异常->" + downURLJson);
                             return;
                         }
                     } catch (Exception ignored) {
-                        promise.fail("Ye: downURLJson格式异常->" + downURLJson);
+                        fail("Ye: downURLJson格式异常->" + downURLJson);
                         return;
                     }
                     String downURL = downURLJson.getJsonObject("data").getString("DownloadURL");
@@ -159,23 +154,21 @@ public class YeTool implements IPanTool {
                             JsonObject res3Json = res3.bodyAsJsonObject();
                             try {
                                 if (res3Json.getInteger("code") != 0) {
-                                    promise.fail("Ye: downUrl2返回值异常->" + res3Json);
+                                    fail("Ye: downUrl2返回值异常->" + res3Json);
                                     return;
                                 }
                             } catch (Exception ignored) {
-                                promise.fail("Ye: downUrl2格式异常->" + downURLJson);
+                                fail("Ye: downUrl2格式异常->" + downURLJson);
                                 return;
                             }
 
                             promise.complete(res3Json.getJsonObject("data").getString("redirect_url"));
 
-                        }).onFailure(t -> promise.fail(PanExceptionUtils.fillRunTimeException("Ye",
-                                reqBodyJson.encodePrettily(), t)));
+                        }).onFailure(this.handleFail("获取直链失败"));
 
                     } catch (MalformedURLException e) {
-                        promise.fail("urlParams解析异常" + e.getMessage());
+                        fail("urlParams解析异常" + e.getMessage());
                     }
-                }).onFailure(t -> promise.fail(PanExceptionUtils.fillRunTimeException("Ye",
-                        reqBodyJson.encodePrettily(), t)));
+                }).onFailure(this::handleFail);
     }
 }
