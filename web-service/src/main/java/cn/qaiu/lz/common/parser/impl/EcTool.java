9bc7e9aa26480ca89f84031145a6059ef24bc281
qaiu <736226400@qq.com>
2023-08-10 01:38:20 +0800

    代码结构优化 修复123pan解析错误的问题#9

--- web-service/src/main/java/cn/qaiu/lz/common/parser/impl/EcTool.java
+++ web-service/src/main/java/cn/qaiu/lz/common/parser/impl/EcTool.java
@@ -1,22 +1,17 @@
 package cn.qaiu.lz.common.parser.impl;
 
 import cn.qaiu.lz.common.parser.IPanTool;
+import cn.qaiu.lz.common.parser.PanBase;
 import cn.qaiu.lz.common.util.CommonUtils;
-import cn.qaiu.lz.common.util.PanExceptionUtils;
-import cn.qaiu.vx.core.util.VertxHolder;
 import io.vertx.core.Future;
-import io.vertx.core.Promise;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.web.client.WebClient;
 import io.vertx.uritemplate.UriTemplate;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * 移动云空间解析
  */
-@Slf4j
-public class EcTool implements IPanTool {
+public class EcTool extends PanBase implements IPanTool {
     private static final String FIRST_REQUEST_URL = "https://www.ecpan.cn/drive/fileextoverrid" +
             ".do?chainUrlTemplate=https:%2F%2Fwww.ecpan" +
             ".cn%2Fweb%2F%23%2FyunpanProxy%3Fpath%3D%252F%2523%252Fdrive%252Foutside&parentId=-1&data={dataKey}";
@@ -25,10 +20,12 @@ public class EcTool implements IPanTool {
 
     public static final String SHARE_URL_PREFIX = "www.ecpan.cn/";
 
-    public Future<String> parse(String data, String code) {
-        String dataKey = CommonUtils.adaptShortPaths(SHARE_URL_PREFIX, data);
-        Promise<String> promise = Promise.promise();
-        WebClient client = WebClient.create(VertxHolder.getVertxInstance());
+    public EcTool(String key, String pwd) {
+        super(key, pwd);
+    }
+
+    public Future<String> parse() {
+        String dataKey = CommonUtils.adaptShortPaths(SHARE_URL_PREFIX, key);
         // 第一次请求 获取文件信息
         client.getAbs(UriTemplate.of(FIRST_REQUEST_URL)).setTemplateParam("dataKey", dataKey).send().onSuccess(res -> {
                     JsonObject jsonObject = res.bodyAsJsonObject();
@@ -37,8 +34,7 @@ public class EcTool implements IPanTool {
                             .getJsonObject("var")
                             .getJsonObject("chainFileInfo");
                     if (fileInfo.containsKey("errMesg")) {
-                        promise.fail(new RuntimeException(DOWNLOAD_REQUEST_URL + " 解析失败: "
-                                + fileInfo.getString("errMesg")) + " key = " + dataKey);
+                        fail("{} 解析失败:{} key = {}", FIRST_REQUEST_URL, fileInfo.getString("errMesg"), dataKey);
                         return;
                     }
                     JsonObject cloudpFile = fileInfo.getJsonObject("cloudpFile");
@@ -55,9 +51,9 @@ public class EcTool implements IPanTool {
                         JsonObject jsonRes = res2.bodyAsJsonObject();
                         log.debug("ecPan get download url -> {}", res2.body().toString());
                         promise.complete(jsonRes.getJsonObject("var").getString("downloadUrl"));
-                    }).onFailure(t -> promise.fail(PanExceptionUtils.fillRunTimeException("Ec", dataKey, t)));
+                    }).onFailure(handleFail(""));
                 }
-        ).onFailure(t -> promise.fail(PanExceptionUtils.fillRunTimeException("Ec", dataKey, t)));;
+        ).onFailure(handleFail(FIRST_REQUEST_URL));
         return promise.future();
     }
 }
--- web-service/src/main/java/cn/qaiu/lz/common/parser/impl/FjTool.java
+++ web-service/src/main/java/cn/qaiu/lz/common/parser/impl/FjTool.java
@@ -1,13 +1,12 @@
 package cn.qaiu.lz.common.parser.impl;
 
 import cn.qaiu.lz.common.parser.IPanTool;
+import cn.qaiu.lz.common.parser.PanBase;
 import cn.qaiu.lz.common.util.AESUtils;
 import cn.qaiu.lz.common.util.CommonUtils;
-import cn.qaiu.lz.common.util.PanExceptionUtils;
 import cn.qaiu.vx.core.util.VertxHolder;
 import io.vertx.core.Future;
 import io.vertx.core.MultiMap;
-import io.vertx.core.Promise;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
@@ -20,7 +19,7 @@ import java.util.UUID;
  *
  * @version V016_230609
  */
-public class FjTool implements IPanTool {
+public class FjTool extends PanBase implements IPanTool {
 
     public static final String SHARE_URL_PREFIX = "https://www.feijix.com/s/";
     private static final String API_URL_PREFIX = "https://api.feijipan.com/ws/";
@@ -31,10 +30,13 @@ public class FjTool implements IPanTool {
     private static final String SECOND_REQUEST_URL = API_URL_PREFIX + "file/redirect?downloadId={fidEncode}&enable=1" +
             "&devType=6&uuid={uuid}&timestamp={ts}&auth={auth}";
 
-    public Future<String> parse(String data, String code) {
-        String dataKey = CommonUtils.adaptShortPaths(SHARE_URL_PREFIX, data);
+    public FjTool(String key, String pwd) {
+        super(key, pwd);
+    }
 
-        Promise<String> promise = Promise.promise();
+    public Future<String> parse() {
+        String dataKey = CommonUtils.adaptShortPaths(SHARE_URL_PREFIX, key);
+
         WebClient client = WebClient.create(VertxHolder.getVertxInstance(),
                 new WebClientOptions().setFollowRedirects(false));
         String shareId = String.valueOf(AESUtils.idEncrypt(dataKey));
@@ -44,11 +46,11 @@ public class FjTool implements IPanTool {
         client.postAbs(UriTemplate.of(FIRST_REQUEST_URL)).setTemplateParam("shareId", shareId).send().onSuccess(res -> {
             JsonObject resJson = res.bodyAsJsonObject();
             if (resJson.getInteger("code") != 200) {
-                promise.fail(FIRST_REQUEST_URL + " 返回异常: " + resJson);
+                fail(FIRST_REQUEST_URL + " 返回异常: " + resJson);
                 return;
             }
             if (resJson.getJsonArray("list").size() == 0) {
-                promise.fail(FIRST_REQUEST_URL + " 解析文件列表为空: " + resJson);
+                fail(FIRST_REQUEST_URL + " 解析文件列表为空: " + resJson);
                 return;
             }
             // 文件Id
@@ -67,12 +69,12 @@ public class FjTool implements IPanTool {
                     .setTemplateParam("auth", auth).send().onSuccess(res2 -> {
                         MultiMap headers = res2.headers();
                         if (!headers.contains("Location")) {
-                            promise.fail(SECOND_REQUEST_URL + " 未找到重定向URL: \n" + res.headers());
+                            fail(SECOND_REQUEST_URL + " 未找到重定向URL: \n" + res.headers());
                             return;
                         }
                         promise.complete(headers.get("Location"));
-                    }).onFailure(t -> promise.fail(PanExceptionUtils.fillRunTimeException("Fj", dataKey, t)));
-        }).onFailure(t -> promise.fail(PanExceptionUtils.fillRunTimeException("Fj", dataKey, t)));
+                    }).onFailure(handleFail(SECOND_REQUEST_URL));
+        }).onFailure(handleFail(FIRST_REQUEST_URL));
 
         return promise.future();
     }
