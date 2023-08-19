00b30331eb5b719c288358429bd71340abee53e1
QAIU <736226400@qq.com>
2023-08-19 17:28:30 +0800

    奶牛快传支持下载zip目录分享#11, core框架优化

--- web-service/src/main/java/cn/qaiu/lz/common/parser/impl/LzTool.java
+++ web-service/src/main/java/cn/qaiu/lz/common/parser/impl/LzTool.java
@@ -2,6 +2,7 @@ package cn.qaiu.lz.common.parser.impl;
 
 import cn.qaiu.lz.common.parser.IPanTool;
 import cn.qaiu.lz.common.parser.PanBase;
+import cn.qaiu.lz.common.util.JsExecUtils;
 import cn.qaiu.vx.core.util.VertxHolder;
 import io.vertx.core.Future;
 import io.vertx.core.MultiMap;
@@ -9,7 +10,10 @@ import io.vertx.core.Promise;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
+import org.openjdk.nashorn.api.scripting.ScriptObjectMirror;
 
+import javax.script.ScriptException;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -29,6 +33,42 @@ public class LzTool extends PanBase implements IPanTool {
         super(key, pwd);
     }
 
+    /*
+		var wsk_sign = 'c20230818';
+		var aihidcms = 'Fn5L';
+		var iucjdsd = '';
+		var ws_sign = 'c20230818';
+		var sasign = 'B2EBPw8_bBDVRWFdoAzMGOlI5ATwEbVFgV2NVZwVhATYILld0WjoAZQhoVDsEYlZjBm4BNgVoVGcHMQ_c_c';
+		var ajaxdata = '?ctdf';
+		$.ajax({
+			type : 'post',
+			url : '/ajaxm.php',
+			//b data : { 'action':'downprocess','signs':ajaxdata,'sign':'w},
+			data : { 'action':'downprocess','signs':ajaxdata,'sign':sasign,'websign':iucjdsd,'websignkey':aihidcms,'ves':1 },
+			//b data : { 'action':'downprocess','signs':ajaxdata,'sign':'','websign':ws_sign,'websignkey':wsk_sign,'ves':1 },
+			dataType : 'json',
+			success:function(msg){
+				var date = msg;
+				if(date.zt == '1'){
+										$("#tourl").html("<a href="+date.dom+"/file/"+ date.url +" target=_blank rel=noreferrer><span class=txt>电信下载</span><span class='txt txtc'>联通下载</span><span class=txt>普通下载</span></a>");
+					setTimeout('$("#outime").css("display","block");',1800000);
+									}else{
+					$("#tourl").html("网页超时，请刷新");
+				};
+
+			},
+			error:function(){
+				$("#tourl").html("获取失败，请刷新");
+			}
+
+		});
+     */
+
+    /**
+     * 解析器
+     * @return url String
+     */
+    @SuppressWarnings("unchecked")
     public Future<String> parse() {
         String sUrl = key.startsWith("https://") ? key : SHARE_URL_PREFIX + "/" + key;
 
@@ -41,33 +81,59 @@ public class LzTool extends PanBase implements IPanTool {
             Matcher matcher = compile.matcher(html);
             if (!matcher.find()) {
                 // 没有Iframe说明是加密分享, 匹配sign通过密码请求下载页面
-                Pattern compile2 = Pattern.compile("sign=(\\w{16,})");
-                Matcher matcher2 = compile2.matcher(html);
-                if (!matcher2.find()) {
-                    fail(sUrl + ": sign正则匹配失败, 可能分享已失效");
+                // 去TMD正则
+                String jsText = getJsText(html);
+                try {
+                    ScriptObjectMirror scriptMirror = JsExecUtils.executeDynamicJs(jsText, "down_p");
+                    System.out.println(scriptMirror);
+
+                } catch (ScriptException | NoSuchMethodException e) {
+                    fail(e, "js引擎执行失败");
                     return;
                 }
-                String sign = matcher2.group(1);
-                getDownURL(promise, sUrl, client, sign);
+
+//                getDownURL(promise, sUrl, client, sign);
                 return;
             }
             String iframePath = matcher.group(1);
             client.getAbs(SHARE_URL_PREFIX + iframePath).send().onSuccess(res2 -> {
                 String html2 = res2.bodyAsString();
-                System.out.println(html);
-                Matcher matcher2 = Pattern.compile("'sign'\s*:\s*'(\\w+)'").matcher(html2);
-                if (!matcher2.find()) {
+
+                // 去TMD正则
+                // Matcher matcher2 = Pattern.compile("'sign'\s*:\s*'(\\w+)'").matcher(html2);
+                String jsText = getJsText(html2);
+                if (jsText == null) {
                     fail(SHARE_URL_PREFIX + iframePath + " -> " + sUrl + ": sign正则匹配失败, 可能分享已失效");
                     return;
                 }
-                String sign = matcher2.group(1);
-                getDownURL(promise, sUrl, client, sign);
+                try {
+                    ScriptObjectMirror scriptObjectMirror = JsExecUtils.executeDynamicJs(jsText, null);
+                    getDownURL(promise, sUrl, client, (Map<String, String>) scriptObjectMirror.get("data"));
+                } catch (ScriptException | NoSuchMethodException e) {
+                    fail(e, "js引擎执行失败");
+                }
             }).onFailure(handleFail(SHARE_URL_PREFIX));
         }).onFailure(handleFail(sUrl));
         return promise.future();
     }
 
-    private void getDownURL(Promise<String> promise, String key, WebClient client, String sign) {
+    private String getJsText(String html) {
+        String jsTagStart = "<script type=\"text/javascript\">";
+        String jsTagEnd = "</script>";
+        int index = html.indexOf(jsTagStart);
+        if (index == -1) {
+            return null;
+        }
+        int startPos = index + jsTagStart.length();
+        int endPos = html.indexOf(jsTagEnd, startPos);
+        return html.substring(startPos, endPos);
+    }
+
+    private void getDownURL(Promise<String> promise, String key, WebClient client, Map<String, ?> signMap) {
+        MultiMap map = MultiMap.caseInsensitiveMultiMap();
+        signMap.forEach((k,v)->{
+            map.set(k,v.toString());
+        });
         MultiMap headers = MultiMap.caseInsensitiveMultiMap();
         var userAgent2 = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, " +
                 "like " +
@@ -81,8 +147,7 @@ public class LzTool extends PanBase implements IPanTool {
         String url = SHARE_URL_PREFIX + "/ajaxm.php";
         client.postAbs(url).putHeaders(headers).sendForm(MultiMap
                 .caseInsensitiveMultiMap()
-                .set("action", "downprocess")
-                .set("sign", sign).set("p", pwd)).onSuccess(res2 -> {
+                .setAll(map)).onSuccess(res2 -> {
             JsonObject urlJson = res2.bodyAsJsonObject();
             if (urlJson.getInteger("zt") != 1) {
                 fail(urlJson.getString("inf"));
