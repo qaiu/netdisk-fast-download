aea97caed1961cdfb63a4c499d21bc51a967aff4
qaiu <736226400@qq.com>
2023-08-20 10:29:00 +0800

    修复蓝奏云pan解析错误的问题#12

--- web-service/src/main/resources/js/lz.js
+++ web-service/src/main/resources/js/lz.js
@@ -5,36 +5,42 @@
 var signObj;
 
 
-/*
-  var $,jQuery;
+var $, jQuery;
 
-  $ = jQuery = function(){
-      return new jQuery.fn.init();
-  }
-
-  jQuery.fn = jQuery.prototype = {
-      init:function(){
-        return {
-            focus: function(a) {
-
-            }
-        }
-      }
-  }
-
-  jQuery.fn.init.prototype = jQuery.fn;
- */
-
-// 伪装ajax函数获取关键数据
-var $ = {
-  ajax: function(obj) {
-    signObj = obj
-  }
+$ = jQuery = function () {
+  return new jQuery.fn.init();
 }
+
+jQuery.fn = jQuery.prototype = {
+  init: function () {
+    return {
+      focus: function (a) {
+
+      },
+      keyup: function(a) {
+
+      },
+      ajax: function (obj) {
+        signObj = obj
+      }
+
+    }
+  },
+
+}
+
+jQuery.fn.init.prototype = jQuery.fn;
+
+
+// 伪装jquery.ajax函数获取关键数据
+$.ajax = function (obj) {
+  signObj = obj
+}
+
 var document = {
   getElementById: function (v) {
     return {
-      value : v
+      value: 'v'
     }
-  }
+  },
 }
