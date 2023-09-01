e9e503c6858cdf6badfb31e210c75e0fbc2ebe15
QAIU <736226400@qq.com>
2023-09-01 18:07:21 +0800

    解析用到的js文件改为String方式引入

--- parser/src/main/java/cn/qaiu/util/JsExecUtils.java
+++ parser/src/main/java/cn/qaiu/util/JsExecUtils.java
@@ -7,12 +7,6 @@ import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 /**
  * 执行Js脚本
@@ -21,28 +15,17 @@ import java.nio.file.Paths;
  * @date 2023/7/29 17:35
  */
 public class JsExecUtils {
-    private static final String JS_PATH = "js/ye123.js";
-    private static final String LZ_JS_PATH = "js/lz.js";
-
-    private static final String RES_PATH;
     private static final Invocable inv;
 
     // 初始化脚本引擎
     static {
         ScriptEngineManager engineManager = new ScriptEngineManager();
         ScriptEngine engine = engineManager.getEngineByName("JavaScript"); // 得到脚本引擎
-        //获取文件所在的相对路径
-        URL resource = JsExecUtils.class.getResource("/");
-        if (resource == null) {
-            throw new RuntimeException("js resource path is null");
-        }
-        RES_PATH = resource.getPath();
-        String reader = RES_PATH + JS_PATH;
-        try (FileReader fReader = new FileReader(reader)) {
-            engine.eval(fReader);
-            fReader.close();
+
+        try {
+            engine.eval(JsContent.ye123);
             inv = (Invocable) engine;
-        } catch (IOException | ScriptException e) {
+        } catch (ScriptException e) {
             throw new RuntimeException(e);
         }
     }
@@ -63,23 +46,11 @@ public class JsExecUtils {
             NoSuchMethodException {
         ScriptEngineManager engineManager = new ScriptEngineManager();
         ScriptEngine engine = engineManager.getEngineByName("JavaScript"); // 得到脚本引擎
-        try {
-            //获取文件所在的相对路径
-            Path path;
-            try {
-                path = Paths.get(RES_PATH + LZ_JS_PATH);
-            } catch (RuntimeException ioe) {
-                path = Paths.get(RES_PATH.substring(1) + LZ_JS_PATH);
-            }
-            String jsContent = Files.readString(path) + "\n" + jsText;
-            engine.eval(jsContent);
-            Invocable inv = (Invocable) engine;
-            //调用js中的函数
-            if (StringUtils.isNotEmpty(funName)) {
-                inv.invokeFunction(funName);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        engine.eval(JsContent.lz);
+        Invocable inv = (Invocable) engine;
+        //调用js中的函数
+        if (StringUtils.isNotEmpty(funName)) {
+            inv.invokeFunction(funName);
         }
 
         return (ScriptObjectMirror) engine.get("signObj");
