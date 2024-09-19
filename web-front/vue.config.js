cca3d6b8b9a478ed96317eec47614d7f171efe1b
qaiu <736226400@qq.com>
2024-09-19 05:22:55 +0000

    web-front add yarn.lock

--- web-front/vue.config.js
+++ web-front/vue.config.js
@@ -40,17 +40,17 @@ module.exports = {
         test: /\.js$|\.html$|\.css/, // 匹配文件
         threshold: 10240 // 对超过10k文件压缩
       }),
-      new FileManagerPlugin({  //初始化 filemanager-webpack-plugin 插件实例
-        onEnd: {
-          mkdir: ['./nfd-front'],
-          delete: [   //首先需要删除项目根目录下的dist.zip
-            './nfd-front.zip',
-          ],
-          archive: [ //然后我们选择dist文件夹将之打包成dist.zip并放在根目录
-            {source: './nfd-front', destination: './nfd-front.zip'},
-          ]
-        }
-      })
+      // new FileManagerPlugin({  //初始化 filemanager-webpack-plugin 插件实例
+      //   onEnd: {
+      //     mkdir: ['./nfd-front'],
+      //     delete: [   //首先需要删除项目根目录下的dist.zip
+      //       './nfd-front.zip',
+      //     ],
+      //     archive: [ //然后我们选择dist文件夹将之打包成dist.zip并放在根目录
+      //       {source: './nfd-front', destination: './nfd-front.zip'},
+      //     ]
+      //   }
+      // })
     ]
   },
 
