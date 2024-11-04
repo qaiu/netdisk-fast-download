4666e942b4dddd0b4f8da528e491e27638e1a62c
QAIU <736226400@qq.com>
2024-11-04 18:56:49 +0800

    升级vue3

--- web-front/src/main.js
+++ web-front/src/main.js
@@ -1,19 +1,28 @@
-import Vue from 'vue'
+import * as Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import VueClipboard from 'vue-clipboard2'
-import JsonViewer from 'vue-json-viewer'
+import VueClipboard from 'vue-clipboard3'
+import DirectiveExtensions from './directive'
+
+import JsonViewer from 'vue3-json-viewer'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import "vue3-json-viewer/dist/index.css";
+
+window.$vueApp = Vue.createApp(App)
+
+
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    window.$vueApp.component(key, component)
+}
 
 // Import JsonViewer as a Vue.js plugin
-Vue.use(JsonViewer)
+window.$vueApp.use(JsonViewer)
+window.$vueApp.use(DirectiveExtensions)
 
 // or
 // components: {JsonViewer}
 
-Vue.use(VueClipboard)
-Vue.config.productionTip = false
-Vue.use(ElementUI)
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+window.$vueApp.use(VueClipboard)
+window.$vueApp.use(ElementPlus)
+window.$vueApp.mount('#app')
